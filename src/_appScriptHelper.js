e568ee35d7c146d1c50dc12b5eb8eb9594e23d59
Amir <mfathulqorib97@gmail.com>
2024-04-06 11:32:29 +0700

    first push to git hub

---
+++ src/_appScriptHelper.js
@@ -0,0 +1,146 @@
+const ss = (sheetIdFromFunction) => SpreadsheetApp.openById(sheetIdFromFunction) 
+const timeStamp = Utilities.formatDate(new Date(), 'GMT+7', 'yyyy-MM-dd\' \'HH:mm:ss');
+const userEmail = Session.getActiveUser().getEmail();
+const getSheetsName = (sheetId) => {
+  const data = ss(sheetId).getSheets().map((sheet) => sheet.getName()).sort((a, b) => {
+    // Extract numerical parts from strings
+    const numA = parseInt(a.match(/^\d+/)[0]);
+    const numB = parseInt(b.match(/^\d+/)[0]);
+    
+    // Compare the numerical parts
+    return numA - numB;
+  })
+  return data
+}
+
+const listSheetInformation = {
+  'validasi-1': {
+    sheetId: '1-I9nYqxG9Gmv3YXjd1gXHBxF4RhBwNMSciG1TguVpuI',
+    sheetUploadData: '0. Validasi 1',
+    sheetUploadImage: '1. Dokumentasi Validasi 1',
+    folderUploadImage: '1S7bMu9XpHAMi_A-Dy8V1ztz2zJRFazW7',
+    orderedKeyUploadData: [
+      'nama', 'lead_id', 'area', 'komoditas', 'nomor_kolam', 'jenis_kolam', 'suhu',
+      'dissolved_oxygen', 'ph', 'tds', 'lokasi_kolam', 'luas', 'kedalaman', 'sumber_air',
+      'debit_air', 'padat_tebar', 'hystorical_pond_cycle', 'hystorical_pond_commodity',
+      'numeric_historikal_tebaran', 'satuan_tebaran', 'numeric_historikal_panen',
+      'historikal_doc', 'farmer_status', 'apakah_kolam_kosong', 'apakah_koperatif',
+      'apakah_ada_kandang_hewan', 'apakah_dekat_jurang', 'apakah_dekat_jalan',
+      'apakah_dekat_pemukiman', 'apakah_banyak_predator', 'apakah_ada_listrik',
+      'apakah_pasang_feeder', 'pond_id', 'sumber_benih'
+    ],
+    columnRanges: {
+      area: 'A2:A',
+      'list-petani': 'B2:C',
+      'jenis-kolam': 'F2:F',
+      'komoditas': 'G2:G',
+      'sumber-air': 'H2:H',
+      'debit-air': 'I2:I',
+      'sumber-benih': 'M2:M',
+      'historikal-siklus': 'J2:J',
+      'historikal-komoditas': 'K2:K',
+      'satuan-tebaran': 'N2:N',
+      'farmer-status': 'L2:L'
+    }
+  },
+  'validasi-midway': {
+    sheetId: '1IvGa6UyquKiUCb75TiyKS6AlUTDnkMp3MBYC77sIjlM',
+    sheetUploadData: '0. Validasi midway',
+    sheetUploadImage: '1. Dokumentasi validasi midway',
+    folderUploadImage: '1ZOpeW_R5wWI3cAHI84yoSJ5x20Ya3iZJ',
+    orderedKeyUploadData: [
+      'nama', 'lead_id', 'area', 'nomor_kolam', 'komoditas', 'tanggal_tebar',
+      'numeric_tebaran', 'satuan_tebaran', 'estimasi_siap_panen', 'satuan_waktu',
+      'numeric_estimasi_panen', 'apakah_sedang_wabah', 'apakah_ikan_kuntet',
+      'apakah_ada_buyer', 'numeric_harga_buyer', 'kontak_buyer', 'pond_id',
+      'apakah_bisa_sampling', 'bobot_sampling_ikan'
+    ],
+    columnRanges: {
+      'area': 'A2:A',
+      'list-petani': 'B2:C',
+      'nomor-kolam': 'D2:F',
+      'komoditas': 'G2:G'
+    }
+  },
+  'validasi-2': {
+    sheetId: '11rvPeZyTncgdfAWk8SnGmv7vYPm-yg5fy52Obpnsi-E',
+    sheetUploadData: '2. Data validasi 2',
+    sheetUploadImage: '3. Dokumentasi validasi 2',
+    folderUploadImage: '1dn-aM6Wa53JlSWtoK2F2_YS9d3iRAaNV',
+    orderedKeyUploadData: [
+      'nama', 'lead_id', 'area', 'nomor_kolam', 'status_kesiapan_kolam',
+      'status_kesiapan_kolam_lainnya', 'estimasi_siap_tebar', 'satuan_waktu',
+      'setFormulaEstimasiTanggalSiapTebar',
+    ],
+    columnRanges: {
+      'area': 'A2:A',
+      'list-petani': 'B2:C',
+      'nomor-kolam': 'D2:F',
+      'status-kesiapan-kolam': 'G2:G'
+    },
+    setFormulaEstimasiTanggalSiapTebar: function(sheet, row, col) {
+      sheet.getRange(row, col).setFormula(
+        `=if(A${row}="","",
+        if(H${row}="Kolam cancel","",
+        if(K${row}="-",C${row},INT(C${row}+if(K${row}="hari",J${row}*1,if(K${row}="minggu",J${row}*7,J${row}*30))))))`
+      );
+    }
+  },
+  'monitoring-tebar': {
+    sheetId: '1qfKDhgIq7y4v6lsbdA24fLH3Ode9SjHED-rV9XDnAmQ',
+    sheetUploadData: '0. Data monitoring tebar',
+    sheetUploadImage: '1. Dokumentasi monitoring tebar',
+    folderUploadImage: '1_-06NknaSxtiIvyb_TZat6RACxjIRIEX',
+    orderedKeyUploadData: [
+      'nama',
+      'lead_id',
+      'area',
+      'komoditas',
+      'nomor_kolam',
+      'tanggal_tebar',
+      'wadah_benih',
+      'jumlah_wadah_benih',
+      'total_benih_per_kolam',
+      'sampling_benih',
+      'apakah_midway',
+      'apakah_benih_efishery'
+    ],
+    columnRanges: {
+      'area': 'A2:A',
+      'list-petani': 'B2:C',
+      'nomor-kolam': 'D2:F',
+      'komoditas': 'G2:G',
+      'wadah-benih': 'H2:H'
+    }
+  },
+  'monitoring-stok-pakan': {
+    sheetId: '1by32xo9MoACo8ZViAdidIRqYiBfUwXtJf1jnGowc8Gk',
+    sheetUploadData: '0. Data Stok Pakan',
+    sheetUploadImage: '1. Dokumentasi Stok Pakan',
+    folderUploadImage: '1DaA8U4MQewkr-XtDautEFRPix8tlVHS7',
+    orderedKeyUploadData: [
+      'nama',
+      'lead_id',
+      'area',
+      'tanggal_cek_pakan',
+      'sku_pakan_1',
+      'qty_pakan_1',
+      'qty_pakan_1_open',
+      'sku_pakan_2',
+      'qty_pakan_2',
+      'qty_pakan_2_open',
+      'sku_pakan_3',
+      'qty_pakan_3',
+      'qty_pakan_3_open',
+      'sku_pakan_4',
+      'qty_pakan_4',
+      'qty_pakan_4_open',
+      'keterangan'
+    ],
+    columnRanges: {
+      'area': 'A2:A',
+      'list-petani': 'B2:C',
+      'sku-pakan': 'I2:I'
+    }
+  }
+};
---
+++ src/appScriptCode.js
@@ -0,0 +1,259 @@
+const userProperties = PropertiesService.getUserProperties();
+const pageMode = userProperties.getProperty('mode');
+const activePage = listSheetInformation[pageMode]
+
+// use regex to search sheet enum_list
+const sheetEnumList = /enum_list/i
+
+const sheetName = getSheetsName(activePage.sheetId || listSheetInformation.Validasi1.sheetId)
+const index = (targetSheetName) => sheetName.findIndex(name => targetSheetName.test(name))
+const sheet = ss(activePage.sheetId || listSheetInformation.Validasi1.sheetId).getSheetByName(sheetName[index(sheetEnumList)]);
+
+
+function doGet(e) {
+  let page = e.parameter.mode || "validasi-1";
+  let html = HtmlService.createTemplateFromFile(`src/page/${page}`).evaluate().setTitle("Tools QC KSE");
+  let htmlOutput = HtmlService.createHtmlOutput(html);
+
+  userProperties.setProperty('mode', page);
+
+  // add metatag to html
+  htmlOutput.addMetaTag('viewport', 'width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0');
+
+  //Replace {{NAVBAR}} with the Navbar content
+  htmlOutput.setContent(htmlOutput.getContent().replace("{{NAVBAR}}",getNavbar(page)));
+  
+  return htmlOutput;
+}
+
+// Create Navigation Bar
+function getNavbar(page) {
+  const scriptURLValidasi1Page = getScriptURL("mode=validasi-1");
+  const scriptURLValidasiMidwayPage = getScriptURL("mode=validasi-midway");
+  const scriptURLValidasi2Page = getScriptURL("mode=validasi-2");
+  const scriptURLMonitoringTebar = getScriptURL("mode=monitoring-tebar");
+  const scriptURLMonitoringStokPakan = getScriptURL("mode=monitoring-stok-pakan");
+
+  const navLinks = [
+    {
+      text: "Validasi 1",
+      url: scriptURLValidasi1Page,
+      isActive: page === "validasi-1"
+    },
+    {
+      text: "Validasi Midway",
+      url: scriptURLValidasiMidwayPage,
+      isActive: page === "validasi-midway"
+    },
+    {
+      text: "Validasi 2",
+      url: scriptURLValidasi2Page,
+      isActive: page === "validasi-2"
+    },
+    {
+      text: "Monitoring Tebar",
+      url: scriptURLMonitoringTebar,
+      isActive: page === "monitoring-tebar"
+    },
+    {
+      text: "Monitoring Stok Pakan",
+      url: scriptURLMonitoringStokPakan,
+      isActive: page === "monitoring-stok-pakan"
+    }
+  ];
+
+  const navbarTemplate = `
+  <nav class="navbar navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand">Tools QC - 2024 🔥</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon" id="trigger-btn"></span>
+      </button>
+      <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel" data-bs-scroll="false" data-bs-backdrop="true">
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Menu</h5>
+          <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+          <ul class="navbar-nav justify-content-end flex-grow-1 pe-3" id="nav-links">
+            ${navLinks.map(link => `
+              <li class="nav-item" data-bs-dismiss="offcanvas">
+                <a class="nav-item nav-link ${link.isActive ? 'active' : ''}" href="${link.url}">${link.text}</a>
+              </li>
+            `).join('')}
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
+  `;
+
+  return navbarTemplate;
+}
+
+// Returns the URL of the Google Apps Script web app
+function getScriptURL(qs = null) {
+  let url = ScriptApp.getService().getUrl()
+  if(qs){
+    if (qs.indexOf("?") === -1) {
+      qs = "?" + qs;
+    }
+    url = url + qs;
+  }
+  return url;
+}
+
+// INCLUDE HTML PARTS, EG. JAVASCRIPT, CSS, OTHER HTML FILES
+function readFile(filePath) {
+  return HtmlService.createHtmlOutputFromFile(filePath).getContent();
+}
+
+function include(filename) {
+  return readFile(filename);
+}
+
+function includeComponent(filename) {
+  return readFile(`src/components/${filename}`);
+}
+
+function includeTitle(filename) {
+  return readFile(`src/components/titles/${filename}`);
+}
+
+function includeFormContent(filename) {
+  return readFile(`src/components/form-contents/${filename}`);
+}
+
+function includeUtils(filename) {
+  return readFile(`src/utils/${filename}`);
+}
+
+function includeService(filename) {
+  return readFile(`src/services/${filename}`);
+}
+
+function includeStyle(filename) {
+  return readFile(`src/styles/${filename}`);
+}
+
+// GET DATA from spreadsheet section
+function getListArea() {
+  const range = activePage.columnRanges['area'];
+  const rawList = sheet.getRange(range).getValues()
+
+  // Use the filter method to create a new array with only non-empty rows
+  const list = rawList
+    .filter(row => row[0])
+    .sort((a, b) => a[0].localeCompare(b[0]))
+
+  return list
+}
+
+function getListFarmers() {
+  const range = activePage.columnRanges['list-petani'];
+  const rawList = sheet.getRange(range).getValues();
+
+  // Use the filter method to create a new array with only non-empty rows
+  const list = rawList
+    .filter(row => row[0])
+    .sort((a, b) => a[0].localeCompare(b[0]))
+
+  return list;
+}
+
+function getListPonds() {
+  const range = activePage.columnRanges['nomor-kolam'];
+  const rawList = sheet.getRange(range).getValues();
+  const filteredList = rawList
+    .filter(row => row[0] !== '')
+    .map(row => [row[0], row[2]]);
+
+  return filteredList;
+}
+
+function getListSkuPakan() {
+  const range = activePage.columnRanges['sku-pakan'];
+  const rawList = sheet.getRange(range).getValues();
+  const list = rawList
+    .filter(String)
+    
+  return list
+}
+
+function getEnumList(id) {
+  const range = activePage.columnRanges[id];
+  const rawList = sheet.getRange(range).getValues();
+  const list = rawList
+    .filter(String)
+    
+  return list
+}
+
+// end of GET DATA from spreadsheet section
+
+// SUBMIT FORM section
+
+function uploadData(formData) {
+  const assessmentId = Utilities.getUuid();
+  const sheetAssessmentKolam = ss(activePage.sheetId).getSheetByName(activePage.sheetUploadData);
+  const sheetDokumentasiAssessment = ss(activePage.sheetId).getSheetByName(activePage.sheetUploadImage);
+  const lastRowSheetDokumentasi = sheetDokumentasiAssessment.getRange('B1:B').getValues().filter(String).length;
+  const lastRowSheetAssessment = sheetAssessmentKolam.getLastRow();
+  const lastColumnSheetAssessment = sheetAssessmentKolam.getLastColumn();
+  const { lead_id: leadId, nomor_kolam: pondNumber } = formData;
+  const pondId = `${leadId}${'0'.repeat(4 - pondNumber?.toString().length)}${pondNumber}`;
+  
+  const dataForSheet = [
+    [
+      assessmentId,
+      userEmail,
+      timeStamp,
+      ...activePage.orderedKeyUploadData.map(key => {
+        if (key.match(/^numeric/i)) {
+          return formData[key]?.toString().replace(',', '') || '-';
+        } else if (key === 'pond_id') {
+          return pondId || '-';
+        } else if (key.match(/^setFormula/i)) {
+          return
+        } else {
+          return formData[key] || '-';
+        }
+      })
+    ]
+  ];
+
+  sheetAssessmentKolam.getRange(lastRowSheetAssessment + 1, 1, 1, dataForSheet[0].length).setValues(dataForSheet);
+  sheetDokumentasiAssessment.getRange(lastRowSheetDokumentasi + 1, 1).setValue(assessmentId);
+
+  if(activePage.orderedKeyUploadData.some(key => /^setFormula/.test(key))){
+    const setFormula = activePage.orderedKeyUploadData.filter(key => /^setFormula/.test(key))
+
+    setFormula.forEach((key, index) => {
+      activePage[key](sheetAssessmentKolam, lastRowSheetAssessment + 1, lastColumnSheetAssessment + index)
+    })
+  }
+
+  return dataForSheet
+}
+
+function uploadImages(obj) {
+  const documentationId = Utilities.getUuid();
+  const rootFolder = DriveApp.getFolderById(activePage.folderUploadImage);
+  const sheetDokumentasiValidasi = ss(activePage.sheetId).getSheetByName(activePage.sheetUploadImage);
+  const lastActiveRow = sheetDokumentasiValidasi.getRange('B1:B').getValues().filter(String).length;
+  const folder = rootFolder.createFolder(documentationId);
+
+  sheetDokumentasiValidasi.getRange(lastActiveRow + 1, 2).setValue(documentationId);
+  sheetDokumentasiValidasi.getRange(lastActiveRow + 1, 3).setValue(userEmail);
+  
+  obj.forEach((e, index) => {
+    const blob = Utilities.newBlob(Utilities.base64Decode(e.data), e.mimeType, e.fileName);
+    const url = folder.createFile(blob).getUrl();
+    const lastActiveCol = sheetDokumentasiValidasi.getRange(lastActiveRow + 1, 2, 1, 10).getValues()[0].filter(String).length;
+    sheetDokumentasiValidasi.getRange(lastActiveRow + 1, lastActiveCol + 2).setValue(url || "-");
+  });
+  
+  return obj
+}
+
+// end of SUBMIT FORM section
