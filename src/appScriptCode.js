db4ece6224f344e4d596447793259e8e32ac1111
Amir <mfathulqorib97@gmail.com>
2024-04-09 01:10:07 +0700

    add section upload bencana alam and logbook pakan

--- src/appScriptCode.js
+++ src/appScriptCode.js
@@ -1,28 +1,39 @@
 const userProperties = PropertiesService.getUserProperties();
-const pageMode = userProperties.getProperty('mode');
-const activePage = listSheetInformation[pageMode]
+const pageMode = userProperties.getProperty("mode");
+const activePage = listSheetInformation[pageMode];
 
 // use regex to search sheet enum_list
-const sheetEnumList = /enum_list/i
+const sheetEnumList = /enum_list/i;
 
-const sheetName = getSheetsName(activePage.sheetId || listSheetInformation.Validasi1.sheetId)
-const index = (targetSheetName) => sheetName.findIndex(name => targetSheetName.test(name))
-const sheet = ss(activePage.sheetId || listSheetInformation.Validasi1.sheetId).getSheetByName(sheetName[index(sheetEnumList)]);
-
+const sheetName = getSheetsName(
+  activePage.sheetId || listSheetInformation.Validasi1.sheetId
+);
+const index = (targetSheetName) =>
+  sheetName.findIndex((name) => targetSheetName.test(name));
+const sheet = ss(
+  activePage.sheetId || listSheetInformation.Validasi1.sheetId
+).getSheetByName(sheetName[index(sheetEnumList)]);
 
 function doGet(e) {
   let page = e.parameter.mode || "validasi-1";
-  let html = HtmlService.createTemplateFromFile(`src/page/${page}`).evaluate().setTitle("Tools QC KSE");
+  let html = HtmlService.createTemplateFromFile(`src/page/${page}`)
+    .evaluate()
+    .setTitle("Tools QC KSE");
   let htmlOutput = HtmlService.createHtmlOutput(html);
 
-  userProperties.setProperty('mode', page);
+  userProperties.setProperty("mode", page);
 
   // add metatag to html
-  htmlOutput.addMetaTag('viewport', 'width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0');
+  htmlOutput.addMetaTag(
+    "viewport",
+    "width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0"
+  );
 
   //Replace {{NAVBAR}} with the Navbar content
-  htmlOutput.setContent(htmlOutput.getContent().replace("{{NAVBAR}}",getNavbar(page)));
-  
+  htmlOutput.setContent(
+    htmlOutput.getContent().replace("{{NAVBAR}}", getNavbar(page))
+  );
+
   return htmlOutput;
 }
 
@@ -32,40 +43,42 @@ function getNavbar(page) {
   const scriptURLValidasiMidwayPage = getScriptURL("mode=validasi-midway");
   const scriptURLValidasi2Page = getScriptURL("mode=validasi-2");
   const scriptURLMonitoringTebar = getScriptURL("mode=monitoring-tebar");
-  const scriptURLMonitoringStokPakan = getScriptURL("mode=monitoring-stok-pakan");
+  const scriptURLMonitoringStokPakan = getScriptURL(
+    "mode=monitoring-stok-pakan"
+  );
   const scriptURLMonitoringBudidaya = getScriptURL("mode=monitoring-budidaya");
 
   const navLinks = [
     {
       text: "Validasi 1",
       url: scriptURLValidasi1Page,
-      isActive: page === "validasi-1"
+      isActive: page === "validasi-1",
     },
     {
       text: "Validasi Midway",
       url: scriptURLValidasiMidwayPage,
-      isActive: page === "validasi-midway"
+      isActive: page === "validasi-midway",
     },
     {
       text: "Validasi 2",
       url: scriptURLValidasi2Page,
-      isActive: page === "validasi-2"
+      isActive: page === "validasi-2",
     },
     {
       text: "Monitoring Tebar",
       url: scriptURLMonitoringTebar,
-      isActive: page === "monitoring-tebar"
+      isActive: page === "monitoring-tebar",
     },
     {
       text: "Monitoring Stok Pakan",
       url: scriptURLMonitoringStokPakan,
-      isActive: page === "monitoring-stok-pakan"
+      isActive: page === "monitoring-stok-pakan",
     },
     {
       text: "Monitoring Budidaya",
       url: scriptURLMonitoringBudidaya,
-      isActive: page === "monitoring-budidaya"
-    }
+      isActive: page === "monitoring-budidaya",
+    },
   ];
 
   const navbarTemplate = `
@@ -82,11 +95,17 @@ function getNavbar(page) {
         </div>
         <div class="offcanvas-body">
           <ul class="navbar-nav justify-content-end flex-grow-1 pe-3" id="nav-links">
-            ${navLinks.map(link => `
+            ${navLinks
+              .map(
+                (link) => `
               <li class="nav-item" data-bs-dismiss="offcanvas">
-                <a class="nav-item nav-link ${link.isActive ? 'active' : ''}" href="${link.url}">${link.text}</a>
+                <a class="nav-item nav-link ${
+                  link.isActive ? "active" : ""
+                }" href="${link.url}">${link.text}</a>
               </li>
-            `).join('')}
+            `
+              )
+              .join("")}
           </ul>
         </div>
       </div>
@@ -99,8 +118,8 @@ function getNavbar(page) {
 
 // Returns the URL of the Google Apps Script web app
 function getScriptURL(qs = null) {
-  let url = ScriptApp.getService().getUrl()
-  if(qs){
+  let url = ScriptApp.getService().getUrl();
+  if (qs) {
     if (qs.indexOf("?") === -1) {
       qs = "?" + qs;
     }
@@ -144,55 +163,53 @@ function includeStyle(filename) {
 
 // GET DATA from spreadsheet section
 function getListArea() {
-  const range = activePage.columnRanges['area'];
-  const rawList = sheet.getRange(range).getValues()
-
-  // Use the filter method to create a new array with only non-empty rows
-  const list = rawList
-    .filter(row => row[0])
-    .sort((a, b) => a[0].localeCompare(b[0]))
-
-  return list
-}
-
-function getListFarmers() {
-  const range = activePage.columnRanges['list-petani'];
+  const range = activePage.columnRanges["area"];
   const rawList = sheet.getRange(range).getValues();
 
   // Use the filter method to create a new array with only non-empty rows
   const list = rawList
-    .filter(row => row[0])
-    .sort((a, b) => a[0].localeCompare(b[0]))
+    .filter((row) => row[0])
+    .sort((a, b) => a[0].localeCompare(b[0]));
+
+  return list;
+}
+
+function getListFarmers() {
+  const range = activePage.columnRanges["list-petani"];
+  const rawList = sheet.getRange(range).getValues();
+
+  // Use the filter method to create a new array with only non-empty rows
+  const list = rawList
+    .filter((row) => row[0])
+    .sort((a, b) => a[0].localeCompare(b[0]));
 
   return list;
 }
 
 function getListPonds() {
-  const range = activePage.columnRanges['nomor-kolam'];
+  const range = activePage.columnRanges["nomor-kolam"];
   const rawList = sheet.getRange(range).getValues();
   const filteredList = rawList
-    .filter(row => row[0] !== '')
-    .map(row => [row[0], row[2]]);
+    .filter((row) => row[0] !== "")
+    .map((row) => [row[0], row[2]]);
 
   return filteredList;
 }
 
 function getListSkuPakan() {
-  const range = activePage.columnRanges['sku-pakan'];
+  const range = activePage.columnRanges["sku-pakan"];
   const rawList = sheet.getRange(range).getValues();
-  const list = rawList
-    .filter(String)
-    
-  return list
+  const list = rawList.filter(String);
+
+  return list;
 }
 
 function getEnumList(id) {
   const range = activePage.columnRanges[id];
   const rawList = sheet.getRange(range).getValues();
-  const list = rawList
-    .filter(String)
-    
-  return list
+  const list = rawList.filter(String);
+
+  return list;
 }
 
 // end of GET DATA from spreadsheet section
@@ -201,65 +218,103 @@ function getEnumList(id) {
 
 function uploadData(formData) {
   const assessmentId = Utilities.getUuid();
-  const sheetAssessmentKolam = ss(activePage.sheetId).getSheetByName(activePage.sheetUploadData);
-  const sheetDokumentasiAssessment = ss(activePage.sheetId).getSheetByName(activePage.sheetUploadImage);
-  const lastRowSheetDokumentasi = sheetDokumentasiAssessment.getRange('B1:B').getValues().filter(String).length;
+  const sheetAssessmentKolam = ss(activePage.sheetId).getSheetByName(
+    activePage.sheetUploadData
+  );
+  const sheetDokumentasiAssessment = ss(activePage.sheetId).getSheetByName(
+    activePage.sheetUploadImage
+  );
+  const lastRowSheetDokumentasi = sheetDokumentasiAssessment
+    .getRange("B1:B")
+    .getValues()
+    .filter(String).length;
   const lastRowSheetAssessment = sheetAssessmentKolam.getLastRow();
   const lastColumnSheetAssessment = sheetAssessmentKolam.getLastColumn();
   const { lead_id: leadId, nomor_kolam: pondNumber } = formData;
-  const pondId = `${leadId}${'0'.repeat(4 - pondNumber?.toString().length)}${pondNumber}`;
-  
+  const pondId = `${leadId}${"0".repeat(
+    4 - pondNumber?.toString().length
+  )}${pondNumber}`;
+
   const dataForSheet = [
     [
       assessmentId,
       userEmail,
       timeStamp,
-      ...activePage.orderedKeyUploadData.map(key => {
+      ...activePage.orderedKeyUploadData.map((key) => {
         if (key.match(/^numeric/i)) {
-          return formData[key]?.toString().replace(',', '') || '-';
-        } else if (key === 'pond_id') {
-          return pondId || '-';
+          return formData[key]?.toString().replace(",", "") || "-";
+        } else if (key === "pond_id") {
+          return pondId || "-";
         } else if (key.match(/^setFormula/i)) {
-          return
+          return;
         } else {
-          return formData[key] || '-';
+          return formData[key] || "-";
         }
-      })
-    ]
+      }),
+    ],
   ];
 
-  sheetAssessmentKolam.getRange(lastRowSheetAssessment + 1, 1, 1, dataForSheet[0].length).setValues(dataForSheet);
-  sheetDokumentasiAssessment.getRange(lastRowSheetDokumentasi + 1, 1).setValue(assessmentId);
+  sheetAssessmentKolam
+    .getRange(lastRowSheetAssessment + 1, 1, 1, dataForSheet[0].length)
+    .setValues(dataForSheet);
+  sheetDokumentasiAssessment
+    .getRange(lastRowSheetDokumentasi + 1, 1)
+    .setValue(assessmentId);
 
-  if(activePage.orderedKeyUploadData.some(key => /^setFormula/.test(key))){
-    const setFormula = activePage.orderedKeyUploadData.filter(key => /^setFormula/.test(key))
+  if (activePage.orderedKeyUploadData.some((key) => /^setFormula/.test(key))) {
+    const setFormula = activePage.orderedKeyUploadData.filter((key) =>
+      /^setFormula/.test(key)
+    );
 
     setFormula.forEach((key, index) => {
-      activePage[key](sheetAssessmentKolam, lastRowSheetAssessment + 1, lastColumnSheetAssessment + index)
-    })
+      activePage[key](
+        sheetAssessmentKolam,
+        lastRowSheetAssessment + 1,
+        lastColumnSheetAssessment + index
+      );
+    });
   }
 
-  return dataForSheet
+  return dataForSheet;
 }
 
 function uploadImages(obj) {
   const documentationId = Utilities.getUuid();
   const rootFolder = DriveApp.getFolderById(activePage.folderUploadImage);
-  const sheetDokumentasiValidasi = ss(activePage.sheetId).getSheetByName(activePage.sheetUploadImage);
-  const lastActiveRow = sheetDokumentasiValidasi.getRange('B1:B').getValues().filter(String).length;
+  const sheetDokumentasiValidasi = ss(activePage.sheetId).getSheetByName(
+    activePage.sheetUploadImage
+  );
+  const lastActiveRow = sheetDokumentasiValidasi
+    .getRange("B1:B")
+    .getValues()
+    .filter(String).length;
   const folder = rootFolder.createFolder(documentationId);
 
-  sheetDokumentasiValidasi.getRange(lastActiveRow + 1, 2).setValue(documentationId);
+  sheetDokumentasiValidasi
+    .getRange(lastActiveRow + 1, 2)
+    .setValue(documentationId);
   sheetDokumentasiValidasi.getRange(lastActiveRow + 1, 3).setValue(userEmail);
-  
+
   obj.forEach((e, index) => {
-    const blob = Utilities.newBlob(Utilities.base64Decode(e.data), e.mimeType, e.fileName);
+    const blob = Utilities.newBlob(
+      Utilities.base64Decode(e.data),
+      e.mimeType,
+      e.fileName
+    );
     const url = folder.createFile(blob).getUrl();
-    const lastActiveCol = sheetDokumentasiValidasi.getRange(lastActiveRow + 1, 2, 1, 10).getValues()[0].filter(String).length;
-    sheetDokumentasiValidasi.getRange(lastActiveRow + 1, lastActiveCol + 2).setValue(url || "-");
+    const lastActiveCol = sheetDokumentasiValidasi
+      .getRange(lastActiveRow + 1, 2, 1, 10)
+      .getValues()[0]
+      .filter(String).length;
+
+    if (e.type === "dokumentasi") {
+      sheetDokumentasiValidasi
+        .getRange(lastActiveRow + 1, lastActiveCol + 2)
+        .setValue(url || "-");
+    }
   });
-  
-  return obj
+
+  return obj;
 }
 
 // end of SUBMIT FORM section
